2d81aad135f4ffddc930f0ca8c509229618117da
Kuznetsov Danil <kuznetsovdanil.dka@gmail.com>
2022-07-27 16:54:59 +0700

    www

--- src/components/HashtagPostsListComponent.vue
+++ src/components/HashtagPostsListComponent.vue
@@ -10,7 +10,7 @@
   </form>
   <div class="post-item" v-for="post in posts" :key="post.id">
     <div class="datefield">
-      Создан: {{post.created_at}} | Автор: {{post.user_id}}
+      Создан: {{post.created_at}} | Автор: {{post.user.login}}
     </div>
     <div>{{post.body}}</div>
   </div>
